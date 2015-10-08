5ec126579da179b66b877b7fe488dc3d6eac5884
fuex <omar.mahili@gmail.com>
2015-10-08 23:38:27 +0200

    parameter seatTypes is checked, colors and types has to be different from each other, converted all fields to properties to simplify the value checking

--- src/js/seatchart.js
+++ src/js/seatchart.js
@@ -52,14 +52,73 @@ function seatchartJS(seatMap, seatTypes) {
         return (w1 - w2); 
     }; 
     
+    function colorToHex(color) {
+        var colors = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+"beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887","cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff","darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f","darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1","darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff","firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff","gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f","honeydew":"#f0fff0","hotpink":"#ff69b4","indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c","lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2","lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de","lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6","magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee","mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5","navajowhite":"#ffdead","navy":"#000080","oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6","palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1","saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4","tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0","violet":"#ee82ee","wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5","yellow":"#ffff00","yellowgreen":"#9acd32"};
+
+        var hex = colors[color.toLowerCase()];
+        
+        return hex != undefined ? hex : color;
+    };
+    
+    // check the given input
+    for (var i = 0; i < seatTypes.length; i++) {
+        for (var j = i + 1; j < seatTypes.length; j++) {
+            if (seatTypes[i].type.capitalizeFirstLetter() == seatTypes[j].type.capitalizeFirstLetter())
+                throw "seatTypes in seatChartJs: '{0}' and '{1}' equals, types has to be different from each other. Types are case insensitive.".format(seatTypes[i].type, seatTypes[j].type);
+            else if (colorToHex(seatTypes[i].color) == colorToHex(seatTypes[j].color))
+                throw "seatTypes in seatChartJs: '{0}' and '{1}' equals, colors has to be different from each other to be recognized, by the user.".format(seatTypes[i].color, seatTypes[j].color);
+        }
+    }
+    
     // the currency used
-    this.currency = "€";
+    var currency = "€";
     // path where assets are located
-    this.assetsSrc = "assets";
-    this.soundEnabled = true;
+    var assetsSrc = "assets";
+    var soundEnabled = true;
     // shopping cart size
-    this.shoppingCartWidth = 200;
-    this.shoppingCartHeight = 200;
+    var shoppingCartWidth = 200;
+    var shoppingCartHeight = 200;
+    
+    this.setCurrency = function (value) {
+        self.currency = value;
+    };
+    
+    this.getCurrency = function () {
+        return self.currency;
+    };
+    
+    this.setAssetsSrc = function (value) {
+        self.assetsSrc = value;
+    };
+    
+    this.getAssetsSrc = function () {
+        return self.assetsSrc;
+    };
+    
+    this.setSoundEnabled = function (value) {
+        self.soundEnabled = value;
+    };
+    
+    this.getSoundEnabled = function () {
+        return self.soundEnabled;
+    };
+    
+    this.setShoppingCartWidth = function (value) {
+        self.shoppingCartWidth = value;
+    };
+    
+    this.getShoppingCartWidth = function () {
+        return self.shoppingCartWidth;
+    };
+    
+    this.setShoppingCartHeight = function (value) {
+        self.shoppingCartHeight = value;
+    };
+    
+    this.getShoppingCartHeight = function () {
+        return self.shoppingCartHeight;
+    };
     
     var self = this;
     var alphabet = 'ABCDEFGHIJLMNOPQRSTUVWXYZ';
@@ -552,7 +611,7 @@ function seatchartJS(seatMap, seatTypes) {
         var item = document.createElement("div");
         item.className = "seatChart-sc-item";
         //-2 because of the item left padding
-        item.style.width = "{0}px".format(self.shoppingCartWidth - getScrollBarWidth() - 2);
+        item.style.width = "{0}px".format(shoppingCartWidth - getScrollBarWidth() - 2);
         item.setAttribute("id", "item-{0}".format(id));
         
         var desc = document.createElement("p");
@@ -605,8 +664,8 @@ function seatchartJS(seatMap, seatTypes) {
         var shoppingCartTitle = createIconedTitle("Shopping cart", "{0}/icons/shoppingcart.svg".format(self.assetsSrc), "Shopping cart icon.");  
         
         scItemsContainer = createScItemsContainer();
-        scItemsContainer.style.width = "{0}px".format(self.shoppingCartWidth);
-        scItemsContainer.style.height = "{0}px".format(self.shoppingCartHeight);
+        scItemsContainer.style.width = "{0}px".format(shoppingCartWidth);
+        scItemsContainer.style.height = "{0}px".format(shoppingCartHeight);
         var scTotal = createScTotal();
         
         shoppingCartContainer.appendChild(shoppingCartTitle);
