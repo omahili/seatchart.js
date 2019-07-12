79e08c50685a616398a90f1d8dd5ec80f0cf4b08
unknown <omar.mahili@gmail.com>
2019-07-12 21:10:20 +0200

    Update docs

--- scripts/move-files.js
+++ scripts/move-files.js
@@ -10,4 +10,4 @@ fs.copy('scripts/docs.css', 'docs/styles/docs.css', printError);
 fs.copy('src/js/seatchart.js', 'docs/scripts/seatchart.js', printError);
 fs.copy('src/css/seatchart.css', 'docs/styles/seatchart.css', printError);
 fs.copy('src/assets', 'docs/assets', printError);
-fs.copy('examples/example01.html', 'docs/demo.html', printError);
+fs.copy('scripts/demo.html', 'docs/demo.html', printError);
