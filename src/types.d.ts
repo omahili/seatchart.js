6560337f18fad1f9db335e63863470571c3885a9
omarmahili <omar.mahili@hotmail.it>
2020-01-05 03:31:01 +0100

    Set nullable options

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,7 +3,7 @@ type Options = {
         id: string,
         rows: number,
         columns: number,
-        seatNameCallback: (
+        seatName?: (
             row: {
                 index: number,
                 disabled: boolean,
@@ -15,56 +15,56 @@ type Options = {
                 disabledCount: number,
             },
         ) => string,
-        reserved: {
-            seats: Array<number>,
+        reserved?: {
+            seats?: Array<number>,
         },
-        disabled: {
-            seats: Array<number>,
-            rows: Array<number>,
-            columns: Array<number>,
+        disabled?: {
+            seats?: Array<number>,
+            rows?: Array<number>,
+            columns?: Array<number>,
         },
-        indexes: {
-            rows: {
-                visible: boolean,
-                position: 'left' | 'right',
-                name: (
+        indexes?: {
+            rows?: {
+                visible?: boolean,
+                position?: 'left' | 'right',
+                name?: (
                     index: number,
                     disabled: boolean,
                     disabledCount: number,
                 ) => string,
             },
-            columns: {
-                visible: boolean,
-                position: 'top' | 'bottom',
-                name: (
+            columns?: {
+                visible?: boolean,
+                position?: 'top' | 'bottom',
+                name?: (
                     index: number,
                     disabled: boolean,
                     disabledCount: number,
                 ) => string,
             },
         },
-        front: {
-            visible: boolean,
+        front?: {
+            visible?: boolean,
         },
     },
     types: Array<{
         type: string,
         backgroundColor: string,
         price: number,
-        textColor: string,
-        selected: Array<number>,
+        textColor?: string,
+        selected?: Array<number>,
     }>,
-    cart: {
+    cart?: {
         id: string,
-        height: string,
-        width: string,
-        currency: number,
+        height?: string,
+        width?: string,
+        currency?: string,
     },
-    legend: {
+    legend?: {
         id: string,
     },
-    assets: {
-        path: string,
+    assets?: {
+        path?: string,
     },
 }
 
