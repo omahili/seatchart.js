282aa092ea1eedba01e0ee9594bc6ad939d102f7
omarmahili <omar.mahili@hotmail.it>
2020-01-11 11:52:30 +0100

    Add and use gulp to watch and build source code

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const rollup = require('rollup');
+const rollupTypescript = require('@rollup/plugin-typescript');
+
+const output = {
+  file: 'dist/seatchart.js',
+  format: 'umd',
+  name: "Seatchart"
+};
+const input = 'src/seatchart.ts';
+const plugins = [rollupTypescript()];
+
+gulp.task('watch', function () {
+  const watcher = rollup.watch({
+    input,
+    output,
+    plugins,
+    watch: [
+      'src'
+    ]
+  });
+
+  console.log('\n' + '\x1b[32m' + 'Watching files...' + '\x1b[0m', '\n');
+
+  watcher.on('event', (event) => {
+    if (event.code === 'BUNDLE_START') {
+      console.log('\x1b[36m' + 'File changed' + '\x1b[0m' + ', bundling', '\x1b[35m' + event.input + '\x1b[0m');
+    } else if (event.code === 'BUNDLE_END') {
+      console.log('\x1b[36m' + 'Project bundled' + '\x1b[0m', 'in', event.duration, 'ms to', '\x1b[35m' + 'dist/seatchart.js' + '\x1b[0m' + '\n');
+    } else if (event.code === 'ERROR') {
+      console.log(event.error + '\n');
+    }
+  });
+
+  return watcher;
+});
+
+gulp.task('default', async function () {
+  console.log('\n' + '\x1b[32m' + 'Build project...' + '\x1b[0m');
+
+  const bundle = await rollup.rollup({
+    input,
+    plugins,
+  });
+
+  await bundle.write(output);
+
+  console.log('\x1b[32m' + 'Built successfully!' + '\x1b[0m', '\n');
+});
