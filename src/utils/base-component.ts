0bd5920050e2973849fd46f59a79c4281fd79f47
omarmahili <omar.mahili@hotmail.it>
2020-02-08 18:30:55 +0100

    Create base component

---
+++ src/utils/base-component.ts
@@ -0,0 +1,9 @@
+class BaseComponent<T extends Element> {
+    public readonly element: T;
+
+    protected constructor(element: T) {
+        this.element = element;
+    }
+}
+
+export default BaseComponent;
